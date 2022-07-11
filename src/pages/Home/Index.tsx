f21db5c65b5b77ed886f9fa4e3d53c0a888eb380
Gabriel Fernandes <gaabezk@gmail.com>
2022-07-11 03:38:19 -0300

    pagina login

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,11 +1,45 @@
-import { useState } from 'react'
-import './style.css'
+import "./style.css";
+import api from "../../services/api";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <div className='body'>
+
+  const [nome,setNome] = useState<string>();
+  const [senha,setSenha] = useState<string>();
+
+  function fazerLogin(e:any) {
+    e.preventDefault();
+    api.post('login', {
+        username: `${nome}`,
+        password: `${senha}`
+    },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+        },
+    )
+        .then(response => {
+            console.log(response.data)
+            console.log("deu certo")
+        })
+        .catch(error => {
+          console.log(error)
+          console.log("deu merda")
+        })
         
-    </div>
-  )
 }
 
+  return (
+    <div className="body">
+      <form  onSubmit={fazerLogin}>
+        <input type="text" placeholder="Usuario" onChange={(e) => setNome(e.target.value)} />
+        <br/>
+        <input type="text" placeholder="Senha" onChange={(e) => setSenha(e.target.value)} />
+        <br/>
+        <input type="submit"/>
+      </form>
+    </div>
+  );
+}
